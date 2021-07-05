5ea1c6de56c42a79d53b2f22535cbf4b5bc70c08
samira hadjri <85124040+samirA999@users.noreply.github.com>
2021-07-05 20:09:07 +0200

    Add files via upload

--- P3/Android/Entrevoisins/app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
+++ P3/Android/Entrevoisins/app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test on Neighbour service
@@ -40,4 +41,30 @@ public class NeighbourServiceTest {
         service.deleteNeighbour(neighbourToDelete);
         assertFalse(service.getNeighbours().contains(neighbourToDelete));
     }
+
+    //Test pour la liste de favoris ajout/retrait/affichage
+
+    @Test
+    public void getListFavNeighbourNotEmpty() {
+        Neighbour neighbour = service.getNeighbours().get(0);
+        service.addFavorite(neighbour);
+        assertFalse(service.getFavoriteNeighbour().isEmpty());
+    }
+
+    @Test
+    public void addNeighbourToFavoriteWithSuccess() {
+        Neighbour neighbouradd = service.getNeighbours().get(0);
+        service.addFavorite(neighbouradd);
+        assertTrue(service.getFavoriteNeighbour().contains(neighbouradd));
+    }
+
+    @Test
+    public void removeNeighbourToFavoriteWithSuccess() {
+        Neighbour neighbourremove = service.getNeighbours().get(0);
+        service.addFavorite(neighbourremove);
+        assertTrue(service.getFavoriteNeighbour().contains(neighbourremove));
+        service.removeFavorite((neighbourremove));
+        assertFalse(service.getFavoriteNeighbour().contains(neighbourremove));
+    }
+
 }
