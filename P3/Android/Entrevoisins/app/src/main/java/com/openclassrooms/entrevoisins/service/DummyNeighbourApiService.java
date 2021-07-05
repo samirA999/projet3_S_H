de7443ff1b3333e7472b00b164cd30e8780caa00
samira hadjri <85124040+samirA999@users.noreply.github.com>
2021-07-05 20:05:50 +0200

    Add files via upload

--- P3/Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/service/DummyNeighbourApiService.java
+++ P3/Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/service/DummyNeighbourApiService.java
@@ -2,15 +2,15 @@ package com.openclassrooms.entrevoisins.service;
 
 import com.openclassrooms.entrevoisins.model.Neighbour;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Dummy mock for the Api
  */
-public class DummyNeighbourApiService implements  NeighbourApiService {
+public class DummyNeighbourApiService implements NeighbourApiService {
 
     private List<Neighbour> neighbours = DummyNeighbourGenerator.generateNeighbours();
-
 
     /**
      * {@inheritDoc}
@@ -30,10 +30,38 @@ public class DummyNeighbourApiService implements  NeighbourApiService {
 
     /**
      * {@inheritDoc}
+     *
      * @param neighbour
      */
     @Override
     public void createNeighbour(Neighbour neighbour) {
         neighbours.add(neighbour);
+    }
+
+
+    @Override
+    public void addFavorite(Neighbour neighbour) {
+        neighbours.get(neighbours.indexOf(neighbour)).setFavorite(true);
+    }
+
+    @Override
+    public Boolean isFavorite(Neighbour neighbour) {
+        return neighbours.get(neighbours.indexOf(neighbour)).getFavorite();
+    }
+
+    @Override
+    public List<Neighbour> getFavoriteNeighbour() {
+        List<Neighbour> favoriteNeighbourList = new ArrayList<>();
+        for (Neighbour n : neighbours) {
+            if (n.getFavorite()) {
+                favoriteNeighbourList.add(n);
+            }
+        }
+        return favoriteNeighbourList;
+    }
+
+    @Override
+    public void removeFavorite(Neighbour neighbour) {
+        neighbours.get(neighbours.indexOf(neighbour)).setFavorite(false);
     }
 }
--- P3/Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/service/NeighbourApiService.java
+++ P3/Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/service/NeighbourApiService.java
@@ -12,19 +12,42 @@ public interface NeighbourApiService {
 
     /**
      * Get all my Neighbours
+     *
      * @return {@link List}
      */
     List<Neighbour> getNeighbours();
 
     /**
      * Deletes a neighbour
+     *
      * @param neighbour
      */
     void deleteNeighbour(Neighbour neighbour);
 
     /**
      * Create a neighbour
+     *
      * @param neighbour
      */
     void createNeighbour(Neighbour neighbour);
+
+    /**
+     * Get all favorite neighbour
+     */
+
+    List<Neighbour> getFavoriteNeighbour();
+
+    Boolean isFavorite(Neighbour neighbour);
+
+    /**
+     * Add Favorite Neighbour
+     */
+    void addFavorite(Neighbour neighbour);
+
+    /**
+     * Remove favorite Neighbour
+     */
+    void removeFavorite(Neighbour neighbour);
+
+
 }
